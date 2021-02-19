0b43e32be9682ce6ae7fc4ee30c4d1f85c8b9b30
vadyvarela <vadyvarela@bonako.com>
2021-02-19 21:55:42 -0100

    Finish clone youtube with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'Arial', 'sans-sarif']
+      },
+      spacing: {
+        '14': '3.5rem',
+        '18': '4.5rem',
+        '68': '16rem',
+        '92': '23rem'
+      },
+      fontSize: {
+        'ste': '.60rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
